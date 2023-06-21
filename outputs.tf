b07c87818478a407fb8d0c7bb3cd642ae29fed8e
Ryan Faircloth <ryan@dss-i.com>
2023-06-21 07:11:23 -0400

    feat: exec outputs
    fix: exec outputs

--- outputs.tf
+++ outputs.tf
@@ -160,3 +160,30 @@ output "identity_namespace" {
     google_container_cluster.primary
   ]
 }
+
+
+
+output "exec_host" {
+  value = "https://${local.cluster_endpoint}"
+  depends_on = [
+    /* Nominally, the endpoint is populated as soon as it is known to Terraform.
+    * However, the cluster may not be in a usable state yet.  Therefore any
+    * resources dependent on the cluster being up will fail to deploy.  With
+    * this explicit dependency, dependent resources can wait for the cluster
+    * to be up.
+    */
+    google_container_cluster.primary,
+    google_container_node_pool.pools,
+  ]
+  sensitive = true
+}
+output "exec_api" {
+  value = "client.authentication.k8s.io/v1beta1"
+}
+output "exec_command" {
+  value = "gke-gcloud-auth-plugin"
+}
+output "exec_args" {
+  sensitive = true
+  value     = []
+}
